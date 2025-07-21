003e1a0f3122fd00859bfef0e4aaee08f1f68a76
mohammedBayoud <163447058+mohammedBayoud@users.noreply.github.com>
2025-07-21 04:49:22 +0300

    Update navbar.php

--- day6/php_project/navbar.php
+++ day6/php_project/navbar.php
@@ -2,15 +2,22 @@
 $user = $_SESSION['user'] ?? '';
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="dashboard.php">Dashboard</a>
-  <div class="collapse navbar-collapse">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item"><a class="nav-link" href="upload.php">Upload Product</a></li>
-      <li class="nav-item"><a class="nav-link" href="gallery.php">Gallery</a></li>
-      <li class="nav-item"><a class="nav-link" href="view_uploads.php">Image Log Table</a></li>
-      <li class="nav-item"><a class="nav-link" href="view_logins.php">Login Log Table</a></li>
-    </ul>
-    <span class="navbar-text mr-5">Welcome <?= htmlspecialchars($user) ?></span>
-    <a href="logout.php" class="btn btn-outline-light btn-sm">Logout</a>
+  <div class="container-fluid">
+    <a class="navbar-brand" href="dashboard.php">Dashboard</a>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item"><a class="nav-link" href="upload.php">Upload Product</a></li>
+        <li class="nav-item"><a class="nav-link" href="gallery.php">Gallery</a></li>
+        <li class="nav-item"><a class="nav-link" href="view_uploads.php">Image Log Table</a></li>
+        <li class="nav-item"><a class="nav-link" href="view_logins.php">Login Log Table</a></li>
+      </ul>
+      <div class="d-flex align-items-center">
+        <span class="navbar-text text-white me-3">
+          Welcome <?= htmlspecialchars($user) ?>
+        </span>
+        <a href="logout.php" class="btn btn-outline-light btn-sm">Logout</a>
+      </div>
+    </div>
+
   </div>
-</nav> 
+</nav>
