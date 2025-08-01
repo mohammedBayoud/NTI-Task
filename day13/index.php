5c540cfac104071ce18faa9ec892edf4efc2663a
mohammedBayoud <163447058+mohammedBayoud@users.noreply.github.com>
2025-08-01 14:23:22 +0300

    Delete day13 directory

--- day13/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'mail.php';
-$mail->setFrom('guide.me.create@gmail.com', 'Mohammed Mohammed Bayoud');
-$mail->addAddress('ahmedabubakr148@gmail.com');
-$mail->Subject = 'Test';
-$mail->send();
-
-?>
